5b99cdf529c8ddabace14e9f5ea2c1ff438d9d93
Pushpesh <pushpesh.srivaastava@gmail.com>
2026-08-11 10:00:00 +0530

    string basic programs

--- 08chapter.c
+++ 08chapter.c
@@ -1,1 +1,35 @@
 //STRINGS
+#include <stdio.h>
+
+// int main() {
+//     char s[10];
+//     // scanf("%s",s);
+//     // printf("%s ",s);    
+//     gets(s);
+//     puts(s);
+//     return 0;
+// }
+
+/*
+2. Write a program to take string as an input from the user using %c and %s and
+confirm that the strings are equal.
+*/
+#include <stdio.h>
+
+int main() {
+    
+    return 0;
+}
+
+
+/*
+3. Write your own version of strlen function from <string.h> .
+4. Write a function slice() to slice a string. It should change the original string such
+that it is now the sliced string. Take m and n as the start and ending position for
+slice.
+5. Write your own version of strcpy function from <string.h> .
+6. Write a program to encrypt a string by adding 1 to the ASCII value of its characters.
+7. Write a program to decrypt the string encrypted using encrypt function in problem 6.
+8. Write a program to count the occurrence of a given character in a string.
+9. WAP to check whether the given character is present in the string or not?
+*/
