d066f52052e3724d2e19dcc6b6a747f3ff29a536
Pushpesh <pushpesh.srivaastava@gmail.com>
2026-08-17 10:48:08 +0530

    Initial Commit

---
+++ cpp_basics.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h> //<- useful header file that includes all standard libraries | useful in cp but not in projects devevlopment
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
