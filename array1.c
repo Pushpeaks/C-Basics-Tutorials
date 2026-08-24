51a5eac2a74d85682e2964a0298715c7a82e53ff
Pushpesh <pushpesh.srivaastava@gmail.com>
2026-08-24 10:21:52 +0530

    Array 1 programs in lab

--- array1.c
+++ array1.c
@@ -1,91 +1,75 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// getting the elements
-// int main(){
-//     int arr[10]; //10 elements will be there in this array
-//     int i;
-//     for(i=0;i<10;i++){
-//         printf("Enter %d element : \n",i+1);
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("PRINTING THE CONTENT OF THE ARRAY\n");
-//     for(i=0;i<10;i++){
-//         printf("%d ",arr[i]);
-//     }
-// }
+int main() {
+    int n, i, search, found = 0, id = -1;
+    int sum = 0;
+    float average;
 
-//sum and avg
-// int main(){
-//     int arr[10]; //10 elements will be there in this array
-//     int i,sum=0;
-//     for(i=0;i<10;i++){
-//         printf("Enter %d element : \n",i+1);
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("PRINTING THE SUM OF THE ELEMENTS IN THE ARRAY\n");
-//     for(i=0;i<10;i++){
-//         sum+=arr[i];
-//     }
-//     printf("Sum : %d\n",sum);
-//     printf("PRINTING THE AVG OF THE ELEMENTS IN THE ARRAY\n");
-//     printf("Average : %d",sum/10);
-// }
+    printf("Enter the total number of elements you want in the array: ");
+    scanf("%d", &n);
 
-//even and odd
-// int main(){
-//     int arr[10]; //10 elements will be there in this array
-//     int i,sum=0;
-//     for(i=0;i<10;i++){
-//         printf("Enter %d element : \n",i+1);
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("PRINTING THE EVEN ELEMENTS IN THE ARRAY\n");
-//     for(i=0;i<10;i++){
-//         if(arr[i]%2==0)
-//         printf("%d ",arr[i]);        
-//     }
-//     printf("\nPRINTING THE ODD ELEMENTS IN THE ARRAY\n");
-//     for(i=0;i<10;i++){
-//         if(arr[i]%2!=0)
-//         printf("%d ",arr[i]);        
-//     }
-// }
-
-
-//reverse
-// int main(){
-//     int arr[10]; //10 elements will be there in this array
-//     int i,sum=0;
-//     for(i=0;i<10;i++){
-//         printf("Enter %d element : \n",i+1);
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("PRINTING THE REVERSED ARRAY\n");
-//     for(i=9;i>=0;i--){
-//         printf("%d ",arr[i]);
-//     }
-// }
-
-//search
-int main(){
-    int arr[10]; //10 elements will be there in this array
-    int i,search,flag=1,id;
-    for(i=0;i<10;i++){
-        printf("Enter %d element : \n",i+1);
-        scanf("%d",&arr[i]);
+    if (n <= 0) {
+        printf("Number of elements must be greater than 0.\n");
+        return 0;
     }
-    printf("Enter the element you want to search : ");
-    scanf("%d",&search);
-    printf("PRINTING THE INDEX OF THE DEMANDED ELEMENT IN THE ARRAY IF IT EXISTS\n");
-    for(i=0;i<10;i++){
-        if(arr[i]==search){
-            flag=0;
-            id=i; 
-        }              
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        printf("Enter %d element: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
     }
-    if(flag==0){
-        printf("Element found at index %d",id);
-    }else{
-        printf("Element not found");
+
+    printf("\nPRINTING THE CONTENT OF THE ARRAY\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+
+    printf("\n\nPRINTING THE SUM OF THE ELEMENTS IN THE ARRAY\n");
+    printf("Sum: %d\n", sum);
+
+    average = (float)sum / n;
+
+    printf("\nPRINTING THE AVERAGE OF THE ELEMENTS IN THE ARRAY\n");
+    printf("Average: %.2f\n", average);
+
+    printf("\nPRINTING THE EVEN ELEMENTS IN THE ARRAY\n");
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n\nPRINTING THE ODD ELEMENTS IN THE ARRAY\n");
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n\nPRINTING THE REVERSED ARRAY\n");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n\nEnter the element you want to search: ");
+    scanf("%d", &search);
+    for (i = 0; i < n; i++) {
+        if (arr[i] == search) {
+            found = 1;
+            id = i;
+            break;
+        }
+    }
+
+    if (found == 1) {
+        printf("Element found at index %d\n", id);
+    } else {
+        printf("Element not found\n");
+    }
+    return 0;
 }
